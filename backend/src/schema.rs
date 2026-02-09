554beddd7cd7aaf7a06c13ee57c7dfa765517752
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-09 18:20:28 +0530

    added table for ledger

--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,8 +1,22 @@
+use diesel::sql_types::VarChar;
+
 diesel::table! {
     user(id) {
         name -> VarChar,
         id -> Int4,
         password -> VarChar,
+        address -> VarChar,
+        ledger -> Array<ledger>,
         recipients -> Array<VarChar>
     }
+}
+
+diesel::table! {
+    ledger(id){
+        from  -> VarChar,
+        id ->  Int4,
+        to -> Varchar,
+        amount -> BigInt
+    }
+
 }
