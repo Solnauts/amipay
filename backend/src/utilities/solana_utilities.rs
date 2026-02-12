c072c14420da6d5a1b806f7e973a7fb34ac38367
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-12 09:32:32 +0530

    fixed the solana rpc call function

--- backend/src/utilities/solana_utilities.rs
+++ backend/src/utilities/solana_utilities.rs
@@ -1,7 +1,7 @@
 use anchor_client::{
     Client, Cluster,
     solana_sdk::{
-        signature::{Keypair, read_keypair_file},
+        signature::{Keypair, Signature, read_keypair_file},
         signer::Signer,
         system_program,
     },
@@ -10,30 +10,68 @@ use contract::{ID_CONST, accounts, instruction, program::Contract};
 use solana_client::rpc_client::RpcClient;
 use solana_commitment_config::CommitmentConfig;
 use solana_sdk::pubkey::{self, Pubkey};
+use spl_token_interface::id as token_program_value;
 use std::{error::Error, rc::Rc, str::FromStr};
 use tokio::{self};
+
 //call the function in the solana rpc
-pub fn create_user_ata() -> Result<(), Box<dyn Error>> {
+pub struct RpcResponse {
+    pub success: bool,
+    pub value: Signature,
+}
+
+#[tokio::main]
+pub async fn create_user_ata() -> Result<RpcResponse, Box<dyn Error>> {
     //call the solana rpc to call to create the accounts
     let payer = read_keypair_file("~/.config/solana/id.json").unwrap();
-
-    let client = Client::new(Cluster::Devnet, Rc::new(payer));
+    let payer_pubkey = payer.pubkey();
+    let payer_ref: &'static Keypair = Box::leak(Box::new(payer));
+    let client = Client::new(Cluster::Devnet, Rc::new(payer_ref));
 
     let program = client.program(contract::ID).unwrap();
 
     //fetch the main_state_account
-    let main_state_account = get_accounts().unwrap().1;
-anchor_lang::prelude::Pubkey::from_str
+    let required_state_account = get_accounts().unwrap().1;
+    let main_state_account =
+        anchor_lang::prelude::Pubkey::from_str(&required_state_account.to_string()).unwrap();
+
+    //the program id of the program
+    let program_id =
+        anchor_lang::prelude::Pubkey::from_str("HeHSU8GmNjDF7kwM7j2fbheeigdZD9AJzeMC2u5SGCs5")
+            .unwrap();
+
     //fetch usdc_mint
-    let usdc_mint = Pubkey::from_str("USDCoctVLVnvTXBEuP9s8hntucdJokbo17RwHuNXemT").unwrap();
+    let usdc_mint =
+        anchor_lang::prelude::Pubkey::from_str("USDCoctVLVnvTXBEuP9s8hntucdJokbo17RwHuNXemT")
+            .unwrap();
+    let needed_val = token_program_value().to_string();
+    let token_program_id = anchor_lang::prelude::Pubkey::from_str(&needed_val).unwrap();
+    let user_usdc_ata_seed = [b"user_usdc_ata", usdc_mint.as_ref()];
+    let user_usdc_ata_pubkey =
+        anchor_lang::prelude::Pubkey::find_program_address(&user_usdc_ata_seed, &program_id).0;
 
     //place the request on the solana blockchain
-    program.request().accounts(accounts::Initialize {
-        main_state_account,
-        signer: payer.pubkey(),
-        usdc_mint,
-        system_program: system_program::id(),
-    })
+    let rpc_response = program
+        .request()
+        .accounts(accounts::Initialize {
+            main_state_account,
+            signer: payer_pubkey,
+            usdc_mint,
+            token_program: token_program_id,
+            system_program: system_program::id(),
+            user_usdc_ata: user_usdc_ata_pubkey,
+        })
+        .signer(payer_ref)
+        .send()
+        .await
+        .unwrap();
+
+    let response = RpcResponse {
+        success: true,
+        value: rpc_response,
+    };
+
+    Ok(response)
 }
 
 #[tokio::main]
