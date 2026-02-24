5bff03c2d1f01bbc4949160feb1b0bddad3c21e5
its-YogeshChandra <pandittheroyal@gmail.com>
2026-02-25 03:53:01 +0530

    Implement action response handling in the orchestrator and add project READMEs.

--- backend/src/controllers/orchestrator.rs
+++ backend/src/controllers/orchestrator.rs
@@ -1,8 +1,9 @@
 use crate::controllers::wallet_controller::validate_session_token;
 use crate::database::model_functions::conversation_model_function::create_conversation;
+use crate::schema::conversation;
 use crate::utility::orchestrator_message_handler::handle_user_message;
-use crate::utility::{ClientMessage, ErrorPayload, ServerMessage};
-use actix_web::{HttpRequest, HttpResponse, Responder, web};
+use crate::utility::{ClientMessage, ErrorPayload, ServerMessage, handle_action_response};
+use actix_web::{HttpRequest, Responder, web};
 use actix_ws::Message;
 use futures_util::StreamExt as _;
 
@@ -98,7 +99,7 @@ pub async fn main_caller(
                         ClientMessage::ActionResponse(value) => {
                             println!("action response received: {}", value.pending_action_id);
                             // TODO: handle action response
-                            // query the database using payload id
+                            handle_action_response(value, session.clone()).await;
                         }
                     }
                 }
