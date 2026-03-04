d00a5750244f7b97a8cd21df91d32406f6f0a7c9
its-YogeshChandra <pandittheroyal@gmail.com>
2026-03-04 10:13:09 +0530

    feat: Integrate database alias existence checking into the alias generation process.

--- backend/src/utility/auth.rs
+++ backend/src/utility/auth.rs
@@ -1,3 +1,6 @@
+use crate::database::{
+    establish_connection, model::DbAlias, model_functions::alias_model_function::is_alias_exists,
+};
 use petname::petname;
 use rand::{self, Rng};
 
@@ -8,20 +11,26 @@ pub fn create_unique_alias() -> Vec<String> {
     loop {
         let mut alias = petname(2, "").expect("failed to load petname");
         alias = alias.to_lowercase();
-        
+
         //create a random number
         let mut rng = rand::thread_rng();
         let random_number = rng.gen_range(0..100);
-      
-      //create the main suffix 
-      let suffix = "@amipay";
-       let main_alias = format!("{}{}{}", alias, random_number, suffix);
 
-       alias_vec.push(main_alias);
+        //create the main suffix
+        let suffix = "@amipay";
+        let main_alias = format!("{}{}{}", alias, random_number, suffix);
+
+        alias_vec.push(main_alias);
         if alias_vec.len() == 10 {
             break;
         }
     }
 
-    return alias_vec;
+    //call is_alias_exist function to check if the alias exists in the database
+    let mut conn = establish_connection().unwrap();
+
+    //call the database function to check the alias vec exist or not
+    let main_alias_vec = is_alias_exists(&mut conn, alias_vec);
+
+    main_alias_vec
 }
